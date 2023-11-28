65b1776dacc370b20c21c20b9ac56dec1669d171
Chaparro Agustin <achaparro@arsat.com.ar>
2023-11-28 17:07:38 -0300

    h

--- 02-Bases/src/apps.js
+++ 02-Bases/src/apps.js
@@ -1,5 +1,7 @@
 
 const { emailTemplate } = require('./js-Foundation/01-Template');
 
+require('./js-Foundation/02-Destructuring');
+// console.log(emailTemplate);
 
-console.log(emailTemplate);
+
---
+++ 02-Bases/src/js-Foundation/02-Destructuring.js
@@ -0,0 +1,18 @@
+
+
+//console.log(process.env);
+
+const{ DADIR } = process.env;
+
+// console.log(DADIR);
+
+const characters =['flash', 'batman','Green Lantern', 'Superman'];
+
+const [, ,batman] = characters;
+
+// console.log(batman);
+
+
+
+
+
