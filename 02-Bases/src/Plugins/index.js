bf15c7b3df84ea6955ee52811f6b06896ac1a517
ChaparroAgustin <chaparroagustin91@hotmail.com>
2023-12-05 16:29:07 -0300

    factory Function

---
+++ 02-Bases/src/Plugins/index.js
@@ -0,0 +1,7 @@
+const { getAge } = require('../Plugins/get-age.plugin')
+const { getUUID } = require ('../Plugins/uuid.plugin');
+
+module.exports = {
+    getUUID,
+    getAge,
+}
--- 02-Bases/src/js-Foundation/05-Factory.js
+++ 02-Bases/src/js-Foundation/05-Factory.js
@@ -1,16 +1,18 @@
 //Factory function es una funcion que crea un funcion
-
+const { getAge, getUUID } = require('../Plugins')
 const obj = {name: 'John', birthdate:'1991-10-01' };
+
+
 
 
 const buildPerson = ({name, birthdate}) => {
 
 
     return {
-        id: new Date().getTime(),
+        id: getUUID(),
         name: name,
         birthdate: birthdate,
-        age: new Date().getFullYear() - new Date(birthdate).getFullYear(),
+        age: getAge(birthdate)
     }
 
 }
