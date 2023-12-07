9f163e88b1c5adcdf118e7ae4b458c21b4e83085
Chaparro Agustin <achaparro@arsat.com.ar>
2023-12-07 15:42:10 -0300

    Factory Functions

--- 02-Bases/src/apps.js
+++ 02-Bases/src/apps.js
@@ -1,13 +1,13 @@
 console.clear();
 // const { emailTemplate } = require('./js-Foundation/01-Template');
 
-const { getUserById } = require('./js-Foundation/03-Callbacks');
+// const { getUserById } = require('./js-Foundation/03-Callbacks');
 
 // require('./js-Foundation/02-Destructuring');
 // // console.log(emailTemplate);
 
 // require('./js-Foundation/04-Arrow');
-require('./js-Foundation/05-Factory');
+// require('./js-Foundation/05-Factory');
 
 // const id = 2;
 
@@ -21,10 +21,17 @@ require('./js-Foundation/05-Factory');
 
 // })
 
+const { getAge, getUUID } = require('./Plugins')
 
+const { buildMakePerson } = require ('./js-Foundation/05-Factory')
 
+const makePerson = buildMakePerson ({ getUUID, getAge })
 
+const obj = {name: 'John', birthdate:'1991-10-01' };
 
+const john = makePerson( obj );
+
+console.log({ john });
 
 
 
--- 02-Bases/src/js-Foundation/05-Factory.js
+++ 02-Bases/src/js-Foundation/05-Factory.js
@@ -1,24 +1,30 @@
 //Factory function es una funcion que crea un funcion
-const { getAge, getUUID } = require('../Plugins')
-const obj = {name: 'John', birthdate:'1991-10-01' };
+//const { getAge, getUUID } = require('../Plugins')
+
+const buildMakePerson = ({getUUID, getAge}) => {
+
+    return ({ name, birthdate }) => {
 
 
+        return {
+            id: getUUID(),
+            name: name,
+            birthdate: birthdate,
+            age: getAge(birthdate)
+        }
 
-
-const buildPerson = ({name, birthdate}) => {
-
-
-    return {
-        id: getUUID(),
-        name: name,
-        birthdate: birthdate,
-        age: getAge(birthdate)
     }
 
 }
 
 
-const jonh = buildPerson(obj);
 
-console.log(jonh);
+// const obj = {name: 'John', birthdate:'1991-10-01' };
 
+// const jonh = buildPerson(obj);
+
+// console.log(jonh);
+
+module.exports = {
+    buildMakePerson,
+}
