b6a35548b7b02158f3f66349c7a67d68c0952538
Chaparro Agustin <achaparro@arsat.com.ar>
2024-01-11 12:36:07 -0300

    end fo testing multiplicationApp

--- 06-MultiplicationApp-Testing/src/app.test.ts
+++ 06-MultiplicationApp-Testing/src/app.test.ts
@@ -1,13 +1,28 @@
-
-
-
+import {ServerApp} from './Presentation/server-app'
 describe('App', () => {
 
 test ( 'shoul be true', () => {
     expect(true).toBe(true);
 });
 
+test('should call Server.run with values', async()=>{
 
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+    process.argv = ['node', 'app.ts' , '-b', '10', '-l','5', '-s', '-n', 'test-file', '-d', 'test-destination'];
+
+    await import('./app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+        base: 10,
+        limit: 5,
+        showTable: true,
+        name: 'test-file',
+        destination: 'test-destination',
+        extension: 'txt'
+})
+
+})
 
 });
 
