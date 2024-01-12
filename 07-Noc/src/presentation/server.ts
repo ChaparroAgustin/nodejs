3885d28fb3dbc18115c22d9e36024e2ffd11576b
ChaparroAgustin <chaparroagustin91@hotmail.com>
2024-01-11 22:21:03 -0300

    End section Noc App

--- 07-Noc/src/presentation/server.ts
+++ 07-Noc/src/presentation/server.ts
@@ -1,21 +1,27 @@
-import { CronJob } from 'cron'
 
-export class Server{
+import { CheckService } from '../domain/use-cases/checks/check-service';
+import { CronService } from './cron/cron-service';
+
+
+
+export class Server {
 
     public static start() { // es static ya que no es necesario instanciar a server para poder acceder a la property.
-        
-        console.log('Server started...')
-        
-        const job = new CronJob(
-            '*/10 * * * * *',
-            ()=>{
-                const date = new Date();
-                console.log('10 second', date);
-            },
-        );
 
-        job.start();
-   }
+        console.log('Server started...')
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                // new CheckService().execute('http://localhost:3000/posts');
+                const url = 'http://localhost:3000/posts'; //'https://google.com';
+                new CheckService(
+                    ()=> console.log(`${url} is ok`),
+                    ( error )=> console.log( error ),
+                ).execute(url);
+            }
+        );
+    }
 
 
 
